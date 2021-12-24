580e99005c1fe3541e337d98bbc3fa96af7bc9f6
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-12-24 02:03:09 -0600

    Redirects to NotFound view if invalid id, added Loading component

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -1,35 +1,45 @@
 <template>
+  <Loading v-if="isLoading"/>
   <div class="container">
     <h2>{{ pokemonDetail.name }} #{{ pokedexNumber }}</h2>
     <img class="img-fluid" :src="pokemonDetail.img" :alt="pokemonDetail.name" />
-    <template
-      v-for="stat in pokemonDetail.stats"
-      :key="stat.stat.name"
-    >
+    <template v-for="stat in pokemonDetail.stats" :key="stat.stat.name">
       <p>{{ stat.stat.name }}: {{ stat.base_stat }}</p>
     </template>
     <template v-for="type in pokemonDetail.types" :key="type.slot">
-        <p>{{type.type.name}}</p>
+      <p>{{ type.type.name }}</p>
     </template>
-    <p>{{pokemonDetail.height / 10}} m</p>
-    <p>{{pokemonDetail.weight / 10}} Kg</p>
+    <p>{{ pokemonDetail.height / 10 }} m</p>
+    <p>{{ pokemonDetail.weight / 10 }} Kg</p>
   </div>
 </template>
 
 <script>
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { onMounted, ref } from "vue";
 import { getPokemonDetail } from "../services/pokemon";
+import Loading from "../components/Loading.vue";
 export default {
   name: "PokemonDetail",
+  components: {
+    Loading,
+  },
   setup() {
     const { id } = useRoute().params;
+    const router = useRouter();
     const pokemonDetail = ref({});
 
+    const isLoading = ref(null);
+
     onMounted(async () => {
-      pokemonDetail.value = await getPokemonDetail(id);
+      isLoading.value = true;
+      //redirects to 404 if the pokemon doesn't exist
+      const pokemon = await getPokemonDetail(id);
+      if (!pokemon) router.push("/404");
+      pokemonDetail.value = pokemon;
+      isLoading.value = false;
     });
-    return { pokemonDetail };
+    return { pokemonDetail, isLoading };
   },
   computed: {
     pokedexNumber() {
