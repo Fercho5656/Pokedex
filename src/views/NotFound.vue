66eed2eef9c6854f965bac048d4fe579499cb7d3
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-12-24 02:02:11 -0600

    404 View

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <h1>404</h1>
+    <h2>Page not found</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style>
+</style>
