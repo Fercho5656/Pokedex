b5ddd45f57606be509460c20d9e5c4d4ac9a5ab4
Fercho5656 <fercho5_5_5_5@hotmail.com>
2021-12-24 02:01:07 -0600

    Created Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <router-link style="" to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link style="" to="/pokemon">Pokemon</router-link>
+      </li>
+      <li>
+        <router-link style="" to="/about">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style scoped>
+nav {
+  background-color: #fff;
+  padding: 10px;
+  border-radius: 0 0 10px 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+  transition: 0.2s ease-in-out;
+}
+
+ul {
+  display: flex;
+  justify-content: space-around;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+  font-family: "Sora", sans-serif;
+  text-decoration: none;
+  color: #000;
+}
+</style>
