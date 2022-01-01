6e272edbe39b82fc711307c925c3d06aba6bdc61
Fercho5656 <fercho5_5_5_5@hotmail.com>
2022-01-01 17:30:32 -0600

    Switch component to turn on/off

---
+++ src/components/Switch.vue
@@ -0,0 +1,38 @@
+<template>
+  <input type="checkbox" class="switch" />
+</template>
+
+<script>
+export default {
+  name: "Switch",
+};
+</script>
+
+<style scoped>
+.switch {
+  --size: 25px;
+  appearance: none;
+  display: flex;
+  justify-content: flex-start;
+  width: calc(var(--size) * 2);
+  height: var(--size);
+  border-radius: 100px;
+  background-color: gainsboro;
+  transition: background-color 0.2s linear;
+}
+
+.switch:checked {
+  background-color: #04a9f1;
+  justify-content: flex-end;
+}
+
+.switch::after {
+  position: absolute;
+  content: "";
+  width: var(--size);
+  height: var(--size);
+  border-radius: 50%;
+  background-color: white;
+  border: 1px solid gainsboro;
+}
+</style>
