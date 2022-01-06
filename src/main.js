f835fbb2e0eddcc5d4ef8e6e8cc833acc8c1baad
Fercho5656 <fercho5_5_5_5@hotmail.com>
2022-01-05 23:37:50 -0600

    Init global state

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes/router'
+import store from './store/index'
+
+store.actions.setTheme(localStorage.getItem('theme') || 'light')
 
 createApp(App).use(router).mount('#app')
+
+
