22d57f1d49bfe9e1267edde1a9850da4c314c4ac
Fercho5656 <fercho5_5_5_5@hotmail.com>
2022-01-12 23:41:06 -0600

    Fixed visual problems

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,61 @@
 <template>
-  <nav>
-    <ul>
-      <li>
-        <router-link style="" to="/">Home</router-link>
-      </li>
-      <li>
-        <router-link style="" to="/pokemon">Pokemon</router-link>
-      </li>
-      <li>
-        <router-link style="" to="/about">About</router-link>
-      </li>
-      <li>
-        <Switch />
-      </li>
-    </ul>
-  </nav>
+  <header>
+    <Anchor class="logo" href="/">
+      <img src="../assets/logo.png" alt="logo" />
+      <p>Pokédex</p>
+    </Anchor>
+    <nav>
+      <ul>
+        <li>
+          <Anchor href="/pokemon">Pokémon</Anchor >
+        </li>
+        <li>
+          <Anchor href="/about">About</Anchor >
+        </li>
+      </ul>
+    </nav>
+    <Switch />
+  </header>
 </template>
 
 <script>
 import Switch from "../components/Switch.vue";
+import Anchor from "../components/Anchor.vue";
 export default {
   name: "Navbar",
   components: {
     Switch,
+    Anchor,
   },
 };
 </script>
 
 <style scoped>
-nav {
+header {
+  width: 100%;
+  margin: auto;
+  height: 10vh;
+  display: flex;
+  align-items: center;
   padding: 10px;
-  border-radius: 0 0 10px 10px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  transition: 0.2s ease-in-out;
+  position: relative;
+}
+
+.logo {
+  display: flex;
+  flex: 1;
+  align-items: center;
+}
+
+.logo > img {
+  width: 50px;
+  height: auto;
+  margin: 0 10px;
+}
+
+nav {
+  flex: 2;
 }
 
 ul {
@@ -44,8 +67,20 @@ li {
   list-style: none;
 }
 
-a {
+li a {
   font-family: "Sora", sans-serif;
   text-decoration: none;
+}
+
+@media screen and (max-width: 425px) {
+  a {
+    font-size: medium;
+  }
+}
+
+@media screen and (max-width: 375px) {
+  a {
+    font-size: small;
+  }
 }
 </style>
