95b9ae21cee35ab3115bff4fd89c7c8b888f08bc
Fercho5656 <fercho5_5_5_5@hotmail.com>
2022-01-12 23:35:05 -0600

    Added container class to create responsive margins

--- src/App.vue
+++ src/App.vue
@@ -27,11 +27,22 @@ export default {
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Sora:wght@400;700&display=swap");
 
+/**Reset Styles */
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  transition: all 0.2s linear;
+}
+
 /**Light theme*/
 :root {
   --background: 255, 255, 255; /* rgb(255, 255, 255) */
   --background-accent: 245, 245, 245; /* rgb(245, 245, 245) */
   --text: 34, 33, 52; /* rgb(34, 33, 52) */
+  --card-bg: 48, 167, 215; /* rgb(48, 167, 215) */
 }
 
 /**Dark theme */
@@ -39,17 +50,13 @@ export default {
   --background: 34, 33, 52; /* rgb(34, 33, 52) */
   --background-accent: 43, 43, 43; /* rgb(43, 43, 43) */
   --text: 255, 255, 255; /* rgb(255, 255, 255) */
+  background-color: rgb(var(--background));
 }
 
-/**Reset Styles */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  transition: color 0.2s linear;
-}
-
+html,
+main,
 body,
+header,
 footer,
 nav {
   background-color: rgb(var(--background));
@@ -69,6 +76,41 @@ span,
 svg {
   font-family: "Sora", sans-serif;
   color: rgb(var(--text));
+}
+
+/** Global Styles */
+.container {
+  width: 100%;
+  padding-right: 0.75rem;
+  padding-left: 0.75rem;
+  margin-right: auto;
+  margin-left: auto;
+}
+
+@media (min-width: 576px) {
+  .container {
+    max-width: 540px;
+  }
+}
+@media (min-width: 768px) {
+  .container {
+    max-width: 720px;
+  }
+}
+@media (min-width: 992px) {
+  .container {
+    max-width: 960px;
+  }
+}
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1140px;
+  }
+}
+@media (min-width: 1400px) {
+  .container {
+    max-width: 1320px;
+  }
 }
 
 a:hover {
