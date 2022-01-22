caa208bb55f2902480b07db57cdbd3289515b303
Fercho5656 <morenomoranfernando@gmail.com>
2022-01-21 22:16:00 -0600

    Removed pokemon img from details

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,15 +1,15 @@
 const ENDPOINT = import.meta.env.VITE_POKEMON_LIST_ENDPOINT
 const IMG_ENDPOINT = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork'
-const DETAILED_IMG_ENDPOINT = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home'
+// const DETAILED_IMG_ENDPOINT = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home'
 
 export const getPokemonList = async () => {
   const response = await window.fetch(ENDPOINT)
   if (response.status === 404) return null
   const data = await response.json()
-  // adds img to pokemon
   data.results.forEach(pokemon => {
     // splits the url to get the id
     const id = pokemon.url.split('/')[6]
+    // adds img to pokemon
     pokemon.img = `${IMG_ENDPOINT}/${id}.png`
     pokemon.number = id
   })
@@ -21,6 +21,6 @@ export const getPokemonDetail = async (id) => {
   if (response.status === 404) return null
   const data = await response.json()
   // adds img to pokemon
-  data.img = `${DETAILED_IMG_ENDPOINT}/${id}.png`
+  // data.img = `${DETAILED_IMG_ENDPOINT}/${id}.png`
   return data
 }
