1780ad10ffbdd813f6d20eae62318c30f6b2a86c
Fercho5656 <morenomoranfernando@gmail.com>
2022-06-05 21:04:38 -0500

    Switched to script setup

--- src/views/PokemonDetail.vue
+++ src/views/PokemonDetail.vue
@@ -18,8 +18,8 @@
       </div>
       <ul class="types">
         <h3>Type</h3>
-        <li v-for="type in pokemonDetail.types" :key="type.slot">
-          <Badge :type="type.type.name" />
+        <li v-for="pokemonType in pokemonDetail.types" :key="pokemonType.slot">
+          <Badge :type="pokemonType.type.name" />
         </li>
       </ul>
     </div>
@@ -27,106 +27,93 @@
 </template>
 
 <script>
+export default {
+  name: "PokemonDetail",
+}
+</script>
+
+<script setup>
 import { useRoute, useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { getPokemonDetail } from "../services/pokemon";
 import Loading from "../components/Loading.vue";
 import InfoCard from "../components/InfoCard.vue";
 import Badge from "../components/Badge.vue";
-export default {
-  name: "PokemonDetail",
-  components: {
-    Loading,
-    InfoCard,
-    Badge,
-  },
-  setup() {
-    const { id } = useRoute().params;
-    const router = useRouter();
-    const pokemonDetail = ref([]);
-    const pokemonSprites = ref();
-    const pokemonAttributes = ref([]);
-    const isLoading = ref(true);
 
-    const isMale = ref(true);
-    const isShiny = ref(false);
-    const pokemonSprite = ref();
+const { id } = useRoute().params;
+const router = useRouter();
+const pokemonDetail = ref([]);
+const pokemonSprites = ref();
+const pokemonAttributes = ref([]);
+const isLoading = ref(true);
 
-    const changeGender = () => {
-      isMale.value = !isMale.value;
-      changeSprite();
-    };
+const isMale = ref(true);
+const isShiny = ref(false);
+const pokemonSprite = ref();
 
-    const changeShiny = () => {
-      isShiny.value = !isShiny.value;
-      changeSprite();
-    };
+const pokedexNumber = computed(() => {
+  return `00${pokemonDetail.value.id}`.slice(-3);
+});
 
-    const changeSprite = () => {
-      isMale.value
-        ? isShiny.value
-          ? (pokemonSprite.value = pokemonSprites.value.front_shiny)
-          : (pokemonSprite.value = pokemonSprites.value.front_default)
-        : isShiny.value
-          ? (pokemonSprite.value =
-            pokemonSprites.value.front_shiny_female ??
-            pokemonSprites.value.front_shiny)
-          : (pokemonSprite.value =
-            pokemonSprites.value.front_female ??
-            pokemonSprites.value.front_default);
-    };
-
-    onMounted(async () => {
-      isLoading.value = true;
-      //redirects to 404 if the pokemon doesn't exist
-      const pokemon = await getPokemonDetail(id);
-      if (!pokemon) router.push("/404");
-      //get the pokemon attributes from the pokemon
-      pokemonAttributes.value = [
-        {
-          name: "Height",
-          value: `${pokemon.height / 10} m`,
-        },
-        {
-          name: "Weight",
-          value: `${pokemon.weight / 10} kg`,
-        },
-      ];
-      pokemonDetail.value = pokemon;
-      pokemonSprites.value = pokemonDetail.value.sprites.other.home;
-      pokemonSprite.value = pokemonSprites.value.front_default;
-      isLoading.value = false;
-    });
-    return {
-      pokemonDetail,
-      isLoading,
-      pokemonAttributes,
-      isMale,
-      isShiny,
-      pokemonSprite,
-      changeGender,
-      changeShiny,
-    };
-  },
-  computed: {
-    pokedexNumber() {
-      return `00${this.pokemonDetail.id}`.slice(-3);
-    },
-  },
+const changeGender = () => {
+  isMale.value = !isMale.value;
+  changeSprite();
 };
+
+const changeShiny = () => {
+  isShiny.value = !isShiny.value;
+  changeSprite();
+};
+
+const changeSprite = () => {
+  isMale.value
+    ? isShiny.value
+      ? (pokemonSprite.value = pokemonSprites.value.front_shiny)
+      : (pokemonSprite.value = pokemonSprites.value.front_default)
+    : isShiny.value
+      ? (pokemonSprite.value =
+        pokemonSprites.value.front_shiny_female ??
+        pokemonSprites.value.front_shiny)
+      : (pokemonSprite.value =
+        pokemonSprites.value.front_female ??
+        pokemonSprites.value.front_default);
+};
+
+onMounted(async () => {
+  isLoading.value = true;
+  //redirects to 404 if the pokemon doesn't exist
+  const pokemon = await getPokemonDetail(id);
+  if (!pokemon) router.push("/404");
+  //get the pokemon attributes from the pokemon
+  pokemonAttributes.value = [
+    {
+      name: "Height",
+      value: `${pokemon.height / 10} m`,
+    },
+    {
+      name: "Weight",
+      value: `${pokemon.weight / 10} kg`,
+    },
+  ];
+  pokemonDetail.value = pokemon;
+  pokemonSprites.value = pokemonDetail.value.sprites.other.home;
+  pokemonSprite.value = pokemonSprites.value.front_default;
+  isLoading.value = false;
+});
 </script>
 
 <style scoped>
 main {
+  width: 100vw;
+  height: 100vh;
   padding: 20px;
 }
 
 .pokemon_info {
   display: grid;
-  grid-template: repeat(2, 1fr) / repeat(2, 1fr);
-  grid-gap: 10px;
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
+  grid-gap: 20px;
   margin-bottom: 20px;
-  place-content: center;
   align-items: center;
   justify-items: center;
 }
@@ -135,7 +122,7 @@ picture {
   position: relative;
 }
 
-picture > input[type="checkbox"] {
+picture>input[type="checkbox"] {
   position: absolute;
   top: 0;
   appearance: none;
@@ -186,6 +173,7 @@ h2,
 h3 {
   text-align: center;
 }
+
 .capitalize {
   text-transform: capitalize;
 }
@@ -209,19 +197,16 @@ li {
   width: 100%;
 }
 
-@media screen and (max-width: 425px) {
-  .pokemon_info {
-    grid-template: repeat(1, 1fr) / repeat(2, 1fr);
-  }
-}
 
 @keyframes flickerBulb {
   0% {
     opacity: 0.5;
   }
+
   50% {
     opacity: 1;
   }
+
   100% {
     opacity: 0.5;
   }
