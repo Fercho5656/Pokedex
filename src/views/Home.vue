2df4a7d7b71a0773c1f2868310024a8605ae8b3b
Fercho5656 <morenomoranfernando@gmail.com>
2022-06-14 15:03:49 -0500

    Shows random pokemon with its pokedex quotes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>This is the Home component.</p>
-  </div>
+  <p>Discover about your favorite</p>
+  <p>Pokémon</p>
+  <button>Search Now</button>
+  <!-- <img :src="pokemon.pokemonDefaultSprite" alt="" srcset=""> -->
+  <PokemonEntries :pokemon-name="pokemon.pokemonName" :pokedex-entries="pokemon.pokedexEntries" />
 </template>
 
-<script>
-export default {
-    name: 'Home'
-};
+<script setup>
+import { reactive } from 'vue'
+import PokemonEntries from '../components/PokemonEntries.vue'
+import usePokemon from '../composables/usePokemon'
+
+const randomPokemonNumber = Math.floor(Math.random() * 905) + 1
+const pokemon = reactive(usePokemon(randomPokemonNumber))
+console.log(pokemon)
 </script>
 
 <style>
